ced1420a066a9b852151dc99585d4a29dc139eae
t.nugent <t.nugent@hotmail.co.uk>
2024-01-05 16:15:26 +0000

    Card and CardList Tailwind formatting

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -15,20 +15,22 @@ const Card: React.FC<Props> = ({
   onPortfolioCreate
 }: Props) : JSX.Element => {
   return (
-    <div className="card">
-        <img 
-            src="https://media.istockphoto.com/id/475676448/photo/shark-fin-above-ocean-water.jpg?s=612x612&w=0&k=20&c=DsszV0Hi_fTf1zrcI-BW19dv-81lcgXojvQ8R5KWg9U="
-            alt="image"
-        />
-
-        <div className="details">
-            <h2>{searchResult.name} ({searchResult.symbol})</h2>
-            <p>{searchResult.currency}</p>
-        </div>
-        <p className="info">
-            {searchResult.exchangeShortName} - {searchResult.stockExchange}
-        </p>
-        <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
+      </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   )
 }
