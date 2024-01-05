e1f78c85c5601b177d07f1ac22b1657defdc0ca0
t.nugent <t.nugent@hotmail.co.uk>
2024-01-05 11:04:38 +0000

    Type narrowing on API return data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,17 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { searchCompanies } from './api';
+import { CompanySearch } from './company';
 
 function App() {
+  // Store the user search
   const [search, setSearch] = useState<string>("");
-  
+  // Storing what we get back from the API
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  // Server error state
+  const [serverError, setServerError] = useState<string>("");
+
   // 'any' allows anything to go into a function
   // Against the purpose of TypeScript
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -14,8 +21,15 @@ function App() {
   }
 
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    // Type checking
+    if(typeof result == "string"){
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log(searchResult);
   };
   
   // Now data is generated in the app
