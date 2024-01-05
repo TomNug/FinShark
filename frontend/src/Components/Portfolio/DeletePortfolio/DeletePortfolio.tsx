06dd06ae214ba7b391192ff7d7c9b8e46e06b333
t.nugent <t.nugent@hotmail.co.uk>
2024-01-05 16:28:51 +0000

    Searchbar, PortfolioLists, Buttons, formatting

--- frontend/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
+++ frontend/src/Components/Portfolio/DeletePortfolio/DeletePortfolio.tsx
@@ -10,7 +10,9 @@ const DeletePortfolio = ({onPortfolioDelete, portfolioValue}: Props) => {
     <div>
         <form onSubmit={onPortfolioDelete}>
             <input readOnly hidden={true} value={portfolioValue} />
-            <button>x</button>
+            <button className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-red-500 hover:text-red-500 hover:bg-white border-red-500">
+          X
+        </button>
         </form>
     </div>
   );
