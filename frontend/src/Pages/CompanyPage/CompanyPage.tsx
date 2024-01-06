890b7ebfba8f4c4a407befd195b12170ab90e523
t.nugent <t.nugent@hotmail.co.uk>
2024-01-06 16:06:53 +0000

    Dashboard for companies

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { CompanyProfile } from '../../company';
 import { getCompanyProfile } from '../../api';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import CompanyDashboard from '../../Components/Dashboard/CompanyDashboard';
+import Tile from '../../Components/Tile/Tile';
 
 interface Props {}
 
@@ -20,7 +23,14 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+
+        <Sidebar />
+        <CompanyDashboard>
+          <Tile title="Company Name" subTitle={company.companyName}></Tile>
+        </CompanyDashboard>
+
+      </div>
       ) : (
         <div>Company not found!</div>
       )}
