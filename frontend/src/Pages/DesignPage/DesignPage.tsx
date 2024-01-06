82d79d38e5bd91b11263f7f9b78ff8b5b44ffdcc
t.nugent <t.nugent@hotmail.co.uk>
2024-01-06 16:29:47 +0000

    Design page, and Table component

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+        <h1>FinShark Design Page</h1>
+        <h2>This is FinShark's design page. Design aspects of the app will be stored here</h2>
+        <Table />
+    </>
+  )
+}
+
+export default DesignPage
