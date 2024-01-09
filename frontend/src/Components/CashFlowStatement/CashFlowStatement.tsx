524afd13465cca48494adda09c54992a40442625
t.nugent <t.nugent@hotmail.co.uk>
2024-01-09 13:15:02 +0000

    Updated some number formatting for readability

--- frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
+++ frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
@@ -4,45 +4,53 @@ import { useOutletContext } from 'react-router';
 import { getCashFlowStatement } from '../../api';
 import Table from '../Table/Table';
 import Spinner from '../Spinner/Spinner';
+import { formatLargeMonetaryNumber } from '../../Helpers/NumberFormatting';
 
 type Props = {}
 
 const config = [
-    {
-      label: "Date",
-      render: (company: CompanyCashFlow) => company.date,
-    },
-    {
-      label: "Operating Cashflow",
-      render: (company: CompanyCashFlow) => company.operatingCashFlow,
-    },
-    {
-      label: "Investing Cashflow",
-      render: (company: CompanyCashFlow) =>
-        company.netCashUsedForInvestingActivites,
-    },
-    {
-      label: "Financing Cashflow",
-      render: (company: CompanyCashFlow) =>
-        company.netCashUsedProvidedByFinancingActivities,
-    },
-    {
-      label: "Cash At End of Period",
-      render: (company: CompanyCashFlow) => company.cashAtEndOfPeriod,
-    },
-    {
-      label: "CapEX",
-      render: (company: CompanyCashFlow) => company.capitalExpenditure,
-    },
-    {
-      label: "Issuance Of Stock",
-      render: (company: CompanyCashFlow) => company.commonStockIssued,
-    },
-    {
-      label: "Free Cash Flow",
-      render: (company: CompanyCashFlow) => company.freeCashFlow,
-    },
-  ];
+  {
+    label: "Date",
+    render: (company: CompanyCashFlow) => company.date,
+  },
+  {
+    label: "Operating Cashflow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.operatingCashFlow),
+  },
+  {
+    label: "Investing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.netCashUsedForInvestingActivites),
+  },
+  {
+    label: "Financing Cashflow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(
+        company.netCashUsedProvidedByFinancingActivities
+      ),
+  },
+  {
+    label: "Cash At End of Period",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.cashAtEndOfPeriod),
+  },
+  {
+    label: "CapEX",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.capitalExpenditure),
+  },
+  {
+    label: "Issuance Of Stock",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.commonStockIssued),
+  },
+  {
+    label: "Free Cash Flow",
+    render: (company: CompanyCashFlow) =>
+      formatLargeMonetaryNumber(company.freeCashFlow),
+  },
+];
 
   
 const CashFlowStatement = (props: Props) => {
