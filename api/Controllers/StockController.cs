d87510bafc78486f042cdeea9e355c503f6eb70e
t.nugent <t.nugent@hotmail.co.uk>
2024-01-10 14:20:37 +0000

    Delete Endpoint for Stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -83,5 +83,22 @@ namespace api.Controllers
 
             return Ok(stockModel.ToStockDto());
         }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete([FromRoute] int id){
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null){
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+
+            _context.SaveChanges();
+
+            // 204
+            return NoContent();
+        }
     }
 }
