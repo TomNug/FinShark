6fc920d45e281245f93da0657b2a80ca5d69409f
t.nugent <t.nugent@hotmail.co.uk>
2024-01-11 10:54:52 +0000

    Creating Comment Repository and Controller. Implemented GetAll.

---
+++ api/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+        public CommentController(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var comments = await _commentRepository.GetAllAsync();
+
+            var commentDto = comments.Select(c => c.ToCommentDto());
+
+            return Ok(commentDto);
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => {
 });
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 // app controls the HTTP request pipeline
 var app = builder.Build();
